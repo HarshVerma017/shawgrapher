2c998600bbf9f475496b0f52078ca11756039828
HarshVerma017 <harshverma17dev@gmail.com>
2025-03-05 18:19:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
-  base: "/shawgrapher/", // Change to your repo name
-  build: {
-    outDir: "dist",
-  },
+  base: "/shawgrapher/", // Set this correctly to match your repo name
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
