500b51d3c177281acd888e7220da5f10922e8754
HarshVerma017 <harshverma17dev@gmail.com>
2025-03-05 18:03:15 +0530

    Fixing build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/shawgrapher/",
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  base: "/shawgrapher/", // Change to your repo name
+  build: {
+    outDir: "dist",
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./dist"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
